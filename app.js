1be0ee618a72246233e3fb9fb0921e747cef667b
kevin-arpe <arpeggiogio@gmail.com>
2020-07-02 14:29:56 +0900

    Finish minor debugging and add detail in ReadMe.

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 const url = require('url');
-const qs = require('querystring');
-const path = require('path');
-const sanitizeHtml = require('sanitize-html');
 const db = require('./lib/db.js');
 const topic = require('./lib/topic.js');
 
 const template = require('./lib/template.js');
-const templateHTML = template.html;
-const templateList = template.list;
-const makeDataCardSet = require('./lib/makecard.js')
 
 db.connect();
 
